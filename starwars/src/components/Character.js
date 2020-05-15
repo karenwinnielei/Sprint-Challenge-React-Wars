d06e6c630b6177f73956161d1afd48eac35cdff9
Karen Lei <karenlei@karens-2019-mbp.lan>
2020-05-15 08:55:30 -0700

    add useEffect and fetch data with axios

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,15 @@
 // Write your Character component here
+import React from 'react'
+import axios from 'axios'
+
+const Character = props => {
+    return (
+        <div>
+            {/* {characterData.map(character => (
+                <h2>{character.name}</h2>
+            ))} */}
+        </div>
+    )
+}
+
+export default Character
